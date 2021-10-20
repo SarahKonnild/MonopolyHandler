3187b077015f272b032a64fa19545f4fe62623fa
Sarah Manon Pradel <S_Pradel@icloud.com>
2021-10-20 14:40:02 +0200

    Added base project files.

---
+++ Monopoly/Objects/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monopoly.Objects
+{
+    public class Player
+    {
+        public string name;
+        public int money;
+        //public List<Properties> properties;
+        //public Token token;
+        public int score;
+
+        public Player(string name, int money, int score //, List<Properties> properties, Token token
+            ) {
+            this.name = name;
+            this.money = money;
+            this.score = score;
+            //this.properties = properties;
+            //this.token = token;
+        }
+    }
+
+
+}
